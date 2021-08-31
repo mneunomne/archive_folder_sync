ab38b78aefeca1e2ea798fdd3e3b3676f27f9c17
mneu windows <alberto.harres@gmail.com>
2021-08-31 20:49:38 +0200

    send osc message

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ require('dotenv').config()
 
 const client = new Client('10.10.48.88', 7400);
 
-client.send('/update', 0, () => {
-  console.log('sent update')
-});
-
 var oscServer = new Server(7400, '10.10.51.64', () => {
   console.log('OSC Server is listening');
 });
@@ -28,24 +24,24 @@ MongoDB
 const mongoUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOSTNAME}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
 
 const Audio = mongoose.model('Audio', mongoose.Schema({
-	id: String,
-	name: String,
-	path: String,
-	text: String,
-	user_id: String,
-	duration: Number,
-	lang: Object
+ id: String,
+ name: String,
+ path: String,
+ text: String,
+ user_id: String,
+ duration: Number,
+ lang: Object
 }))
 
 // MongoDb
 var mongoConnected = false
 mongoose.connect(mongoUri, { useNewUrlParser: true }, function (err, res) {
-	if (err) {
-		console.error(err)
-		throw err
-	}
-	var mongoConnected = true
-	console.log(`[MongoDB] Connected to database "${process.env.MONGODB_DATABASE}"`)
+ if (err) {
+  console.error(err)
+  throw err
+ }
+ var mongoConnected = true
+ console.log(`[MongoDB] Connected to database "${process.env.MONGODB_DATABASE}"`)
 })
 
 const io = require('socket.io-client');
@@ -61,17 +57,19 @@ socket.on('connect', function(socket) {
 
 // on update event
 socket.on('update', function (data) {
-	console.log('[socket.io] received update message', data)
-	Audio.findOne(data, function (err, audio) {
-		console.log('audios', audio)
-		if (fs.existsSync(audio.path)) {
-			// file exists
-		} else {
-			const file = fs.createWriteStream(`audios/${audio.id}.wav`);
-			https.get(audio.path, function(response) {
-				console.log('downloaded ', audio.path)
-				response.pipe(file);
-			});
-		}
-	})
+ console.log('[socket.io] received update message', data)
+ Audio.findOne(data, function (err, audio) {
+  console.log('audios', audio)
+
+   const file = fs.createWriteStream(`audios/${audio.id}.wav`);
+   https.get(audio.path, function(response) {
+    console.log('downloaded ', audio.path)
+    response.pipe(file);
+    setTimeout(() => {
+     client.send('/update', `${audio.id}.wav`, () => {
+      console.log('sent update')
+     });
+    }, 0)
+   });
+ })
 })
